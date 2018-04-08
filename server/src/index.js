da9dda9f4fb86a37e98936275cb433fbf8191a5d
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-04-07 17:18:33 -0700

    "step 1"

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
-// YOUR CODE HERE
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+
+app.use('/', express.static(path.join(__dirname, '../../client/index.html')));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
